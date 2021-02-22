f9655c44b7328e82b4f8cdd9ec9d838d027ee564
N. Prostokvashin <naprostokvashin@sberbank.ru>
2021-02-23 00:31:41 +0300

    change port

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express, { Application } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 
 import connect from "./utils/connect";
 import appRouter from "./src/app.router";
 
-const app: Application = express();
-const PORT: number = 5000 || process.env.PORT;
+const app = express();
+const PORT = 8080 || process.env.PORT;
 
-const db: string = "mongodb+srv://shop:ghjtyubnm@devcluster.taxgx.mongodb.net/RMS?retryWrites=true&w=majority"
+const db = "mongodb+srv://shop:ghjtyubnm@devcluster.taxgx.mongodb.net/RMS?retryWrites=true&w=majority"
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
