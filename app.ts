98219ddf6542b098ca27e3fb07e0a2cf9d7d2af1
N. Prostokvashin <naprostokvashin@sberbank.ru>
2021-02-23 00:25:50 +0300

    change port

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import connect from "./utils/connect";
 import appRouter from "./src/app.router";
 
 const app: Application = express();
-const port: number = 5000 || process.env.PORT;
+const PORT: number = 5000 || process.env.PORT;
 
 const db: string = "mongodb+srv://shop:ghjtyubnm@devcluster.taxgx.mongodb.net/RMS?retryWrites=true&w=majority"
 
@@ -16,6 +16,6 @@ connect(db);
 
 app.use('/api', appRouter)
 
-app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
