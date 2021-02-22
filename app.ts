e2fe08cf4d8e076a66e712899c1430d911ccab95
N. Prostokvashin <naprostokvashin@sberbank.ru>
2021-02-23 00:20:31 +0300

    change port

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import connect from "./utils/connect";
 import appRouter from "./src/app.router";
 
 const app: Application = express();
-const port: number = 5000 || process.env.PORT;
+const port: number = 80 || process.env.PORT;
 
 const db: string = "mongodb+srv://shop:ghjtyubnm@devcluster.taxgx.mongodb.net/RMS?retryWrites=true&w=majority"
 
